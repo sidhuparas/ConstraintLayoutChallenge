1dee84279846cbc077c6f71594186951a6aa5fef
Paras Sidhu <sidhuparas1234@gmail.com>
2018-08-04 20:48:09 +0530

    Code Improvements

--- app/src/main/java/com/parassidhu/constraintlayoutchallenge/MainActivity.java
+++ app/src/main/java/com/parassidhu/constraintlayoutchallenge/MainActivity.java
@@ -1,6 +1,5 @@
 package com.parassidhu.constraintlayoutchallenge;
 
-import android.support.design.widget.AppBarLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -9,15 +8,13 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private String architectsData = "<b>Architects:</b> Pierre Lescot, Claude Perrault, Louis Le Vau";
-    private String establishData = "<b>Established:</b> 10 August 1793";
+    private final String architectsData = "<b>Architects:</b> Pierre Lescot, Claude Perrault, Louis Le Vau";
+    private final String establishData = "<b>Established:</b> 10 August 1793";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        setSupportActionBar((Toolbar) findViewById(R.id.toolbar));
-        getSupportActionBar().setElevation(0);
 
         TextView architects = findViewById(R.id.architects);
         architects.setText(Html.fromHtml(architectsData));
